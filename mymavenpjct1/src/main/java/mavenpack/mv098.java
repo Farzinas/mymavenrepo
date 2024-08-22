2d69bf33dcd5a74fb55d451ab8af2bfef40bfe73
Frazina <Frazina@LAPTOP-T5HRFLI1>
2024-08-22 13:53:17 +0530

    abcd

--- mymavenpjct1/src/main/java/mavenpack/mv098.java
+++ mymavenpjct1/src/main/java/mavenpack/mv098.java
@@ -2,8 +2,9 @@ package mavenpack;
 
 public class mv098 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
