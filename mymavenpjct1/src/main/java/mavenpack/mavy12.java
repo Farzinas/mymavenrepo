ae574400c4ef78d14017e053fa44d1469f71dfd8
Frazina <Frazina@LAPTOP-T5HRFLI1>
2024-08-22 13:13:50 +0530

    hi

--- mymavenpjct1/src/main/java/mavenpack/mavy12.java
+++ mymavenpjct1/src/main/java/mavenpack/mavy12.java
@@ -1,9 +1,13 @@
 package mavenpack;
 
 public class mavy12 {
-
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		int a=10,b=20;
+		int c=a+b;
+		
+		System.out.println(c);
 
 	}
 
