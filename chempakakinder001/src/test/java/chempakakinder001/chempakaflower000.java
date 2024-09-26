a48f930d5e41b3832f67aace9a4e12d87a36237c
Frazina <Frazina@LAPTOP-T5HRFLI1>
2024-09-26 14:26:17 +0530

    first

---
+++ chempakakinder001/src/test/java/chempakakinder001/chempakaflower000.java
@@ -0,0 +1,10 @@
+package chempakakinder001;
+
+public class chempakaflower000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Chempaka Kinder Garten");
+	}
+
+}
---
+++ chempakakinder001/src/test/java/chempakakinder001/chen.java
@@ -0,0 +1,14 @@
+package chempakakinder001;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class chen {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
